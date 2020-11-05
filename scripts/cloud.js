1acd6de3ceaa09292398bc29379a5ed5e31f247b
dacorile <davilucho2009@hotmail.com>
2020-11-05 14:43:02 -0500

    Primer commit pruebas heroku

---
+++ scripts/cloud.js
@@ -0,0 +1,55 @@
+function makeCloud(json){
+	am4core.ready(function() {
+
+	// Themes begin
+	am4core.useTheme(am4themes_animated);
+	// Themes end
+
+	var chart = am4core.create("chartCloudDiv", am4plugins_wordCloud.WordCloud);
+	chart.fontFamily = "Courier New";
+	var series = chart.series.push(new am4plugins_wordCloud.WordCloudSeries());
+	series.randomness = 0.1;
+	series.rotationThreshold = 0.5;
+
+	series.data = json; 
+
+	series.dataFields.word = "tag";
+	series.dataFields.value = "count";
+
+	series.heatRules.push({
+	 "target": series.labels.template,
+	 "property": "fill",
+	 "min": am4core.color("#0000CC"),
+	 "max": am4core.color("#CC00CC"),
+	 "dataField": "value"
+	});
+
+	series.labels.template.url = "https://stackoverflow.com/questions/tagged/{word}";
+	series.labels.template.urlTarget = "_blank";
+	series.labels.template.tooltipText = "{word}: {value}";
+
+	var hoverState = series.labels.template.states.create("hover");
+	hoverState.properties.fill = am4core.color("#FF0000");
+
+	//var subtitle = chart.titles.create();
+	//subtitle.text = "(click to open)";
+
+	var title = chart.titles.create();
+	title.text = "Most Popular Tags @ StackOverflow";
+	title.fontSize = 20;
+	title.fontWeight = "800";
+
+	}); // end am4core.ready()
+}
+
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  var url = "C:/Users/David Ricardo/Desktop/Hackathon Castro y co/statics/pruebaAjax.html";//La url que se carga
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("chartCloudDiv").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("POST", url, true);
+  xhttp.send();
+}
